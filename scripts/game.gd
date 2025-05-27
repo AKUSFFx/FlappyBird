91fb3adf072dc36a95604a9226830b6da5b5a44f
AKUSFFx <axelglki@gmail.com>
2025-05-28 01:26:44 +0200

    add obstacle scene

---
+++ scripts/game.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var obstaculos_escena = preload("res://Escenas/obstacles.tscn")
+
+
+func _on_timer_timeout() -> void:
+	var obstaculos = obstaculos_escena.instantiate()
+	add_child(obstaculos)
